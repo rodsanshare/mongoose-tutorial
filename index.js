cea89f7e08b461c93f082963684d0a4f6c8547d6
Rodney Santiago <rodsan.724@outlook.com>
2018-06-04 10:42:42 -0400

    connecting to atlas

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+require('dotenv/config');
+mongoose.connect(process.env.PRODDB);
 
 const Cat = mongoose.model('Cat', { name: String });
 
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
+
+Cat.find(function (err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
+  })
